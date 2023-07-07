7e93d6dd151ca97b0bd2c9994c3d64d65e2a6b54
Shah Haque <93956445+Shah-Haque@users.noreply.github.com>
2023-07-07 23:26:48 +0100

    Add files via upload

---
+++ WPFMiniProjectApp/WPFMiniProject/AddressEntry.xaml.cs
@@ -0,0 +1,49 @@
+﻿using PersonAddressLibrary.interfaces;
+using PersonAddressLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFMiniProject
+{
+    /// <summary>
+    /// Interaction logic for AddressEntry.xaml
+    /// </summary>
+    public partial class AddressEntry : Window
+    {
+        ISaveAddress _parent;
+
+        public AddressEntry(ISaveAddress parent)
+        {
+            InitializeComponent();
+            _parent = parent;
+
+
+        }
+
+        private void SaveAddress_Click(object sender, RoutedEventArgs e)
+        {
+            AddressModel Address = new AddressModel
+            {
+                StreetAddress = StreetAddressText.Text, // this sets the data field from the model equal to the text field
+                Town = TownText.Text,
+                County = CountyText.Text,
+                PostCode = PostCodeText.Text
+            };
+
+            //This will save the values stored from the full model into the ISAVEADDRESS
+            _parent.SaveAddress(Address);
+
+            //This will close the window
+            this.Close();
+        }
+    }
+}
---
+++ WPFMiniProjectApp/WPFMiniProject/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using PersonAddressLibrary;
+using PersonAddressLibrary.interfaces;
+using PersonAddressLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFMiniProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, ISaveAddress
+    {
+        BindingList<AddressModel> Addresses = new BindingList<AddressModel>();
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            AddressList.ItemsSource = Addresses;
+
+            #region SampleData
+            //Addresses.Add(new AddressModel
+            //{
+            //    StreetAddress = "15 Barlow Road",
+            //    Town = "Wednesbury",
+            //    County = "West Midlands",
+            //    PostCode = "WS10 9QA"
+
+            //});
+            #endregion
+
+
+
+        }
+
+        public void SaveAddress(AddressModel address)
+        {
+           Addresses.Add(address);
+        }
+
+        private void AddAddress_Click(object sender, RoutedEventArgs e)
+        {
+            AddressEntry entry = new AddressEntry(this);
+
+            entry.Show();
+        }
+
+        private void SavePerson_Click(object sender, RoutedEventArgs e)
+        {
+            PersonModel person = new PersonModel
+            {
+                FirstName = FirstNameText.Text,
+                LastName = LastNameText.Text,
+                IsActive = (ActiveCheckBox.IsChecked ?? false),
+                Addresses = Addresses.ToList()
+            };
+
+
+
+        }
+
+
+    }
+}
